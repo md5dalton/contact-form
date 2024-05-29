70ebdcfe4c8fd728dec350de753ba2279d76fb65
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-05-29 14:01:38 +0200

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,52 @@
+import ErrorMsg from "@/components/ErrorMsg"
+import Fieldset from "@/components/Fieldset"
+
 export default () => (
-    <p>page</p>
+    <section className="bg-white mx-4 my-8 rounded-2xl p-6 grid gap-10">
+        <div className="grid gap-8">
+            <h1 className="text-hd">Contact Us</h1>
+            <div className="grid gap-6">
+                <div>
+                    <Fieldset>
+                        <div className="grid gap-2">
+                            <div className="space-x-2">
+                                <label>First name</label>
+                                <span className="text-green-medium">*</span>
+                            </div>
+                            <input className="outline-none border-[1px] border-grey-medium rounded-lg py-3 px-6" type="text" />
+                        </div>
+                        <ErrorMsg>This field is required</ErrorMsg>
+                    </Fieldset>
+                </div>
+                <div>
+                    <Fieldset>
+                        <legend className="space-x-2">
+                            <span>Query Type</span>
+                            <span className="text-green-medium">*</span>
+                        </legend>
+                        <div className="">
+                            <div className="flex gap-4 px-6 py-3  border-[1px] border-grey-medium rounded-lg">
+                                <div className="h-6 w-6 border-2 rounded-full border-grey-medium shrink-0"></div>
+                                <label>Support Request</label>
+                                <input className="hidden" type="radio" />
+                            </div>
+                        </div>
+                        <ErrorMsg>This field is required</ErrorMsg>
+                    </Fieldset>
+                </div>
+            </div>
+        </div>
+        <Fieldset>
+            <div className="flex gap-4 items-center">
+                <div className="h-6 w-6 border-2 rounded-sm border-grey-medium shrink-0"></div>
+                <div className="space-x-2">
+                    <label>I consent to being contacted by the team</label>
+                    <span className="text-green-medium">*</span>
+                </div>
+                <input className="hidden" type="checkbox" />
+            </div>
+            <ErrorMsg>This field is required</ErrorMsg>
+        </Fieldset>
+        <button className="bg-green-medium text-white text-m-b py-4 rounded-lg">Submit</button>
+    </section>
 )
