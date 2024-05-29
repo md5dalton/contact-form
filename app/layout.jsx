d202892d0c51bc4965daab59d53cb2e1b0e7ed2f
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-05-29 14:01:36 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-green-lighter">
       <body className={font.className}>{children}</body>
     </html>
   )
