a1b4fb4700d5d9672c3caeea4f094a9e2ddfad79
md5dalton <md5daltonggs@gmail.com>
2024-06-01 11:42:04 +0200

    Update FieldTextInput.jsx

--- components/FieldTextInput.jsx
+++ components/FieldTextInput.jsx
@@ -1,7 +1,12 @@
+import { gId } from "@/lib/functions"
 import FieldText from "./FieldText"
 
 export default ({ label, type="text" }) => (
     <FieldText label={label}>
-        <input className="outline-none border-[1px] border-grey-medium rounded-lg py-3 px-6" type={type} />
+        <input
+            name={gId(label)}
+            type={type}
+            className="outline-none border-[1px] border-grey-medium rounded-lg py-3 px-6"
+        />
     </FieldText>
 )
