f8e566253586df3d628144e12c28349956c1105c
md5dalton <md5daltonggs@gmail.com>
2024-06-01 11:42:08 +0200

    Update FieldTextarea.jsx

--- components/FieldTextarea.jsx
+++ components/FieldTextarea.jsx
@@ -1,8 +1,10 @@
+import { gId } from "@/lib/functions"
 import FieldText from "./FieldText"
 
 export default ({ label }) => (
     <FieldText label={label}>
         <textarea
+            name={gId(label)}
             className="outline-none border-[1px] border-grey-medium rounded-lg py-3 px-6 min-h-48"
         />
     </FieldText>
