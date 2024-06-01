e6e193868498a9e733f96a192a6f1451cc438a66
md5dalton <md5daltonggs@gmail.com>
2024-06-01 18:36:05 +0200

    Update FieldTextInput.jsx

--- components/FieldTextInput.jsx
+++ components/FieldTextInput.jsx
@@ -1,9 +1,10 @@
 import { gId } from "@/lib/functions"
 import FieldText from "./FieldText"
+import { Field } from "formik"
 
 export default ({ label, type="text" }) => (
     <FieldText label={label}>
-        <input
+        <Field
             name={gId(label)}
             type={type}
             className="outline-none border-[1px] border-grey-medium rounded-lg py-3 px-6"
