3aad16f26476e79192cc5dcff41ebc2588c1c925
md5dalton <md5daltonggs@gmail.com>
2024-06-01 10:32:41 +0200

    Update FieldCheckbox.jsx

--- components/FieldCheckbox.jsx
+++ components/FieldCheckbox.jsx
@@ -3,12 +3,13 @@ import Fieldset from "./Fieldset"
 
 export default ({ label, children }) => (
     <Fieldset>
-        <div className="grid gap-2">
+        <div className="flex gap-4 items-center">
+            <div className="h-6 w-6 border-2 rounded-sm border-grey-medium shrink-0"></div>
             <div className="space-x-2">
                 <label>{label}</label>
                 <span className="text-green-medium">*</span>
             </div>
-            {children}
+            <input className="hidden" type="checkbox" />
         </div>
         <ErrorMsg>This field is required</ErrorMsg>
     </Fieldset>
