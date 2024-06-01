89fdee82a69e81828102c58127a729d63838b033
md5dalton <md5daltonggs@gmail.com>
2024-06-01 18:36:02 +0200

    Update Radio.jsx

--- components/Radio.jsx
+++ components/Radio.jsx
@@ -1,3 +1,4 @@
+import { Field } from "formik"
 import Image from "next/image"
 
 export default ({ label, name }) => {
@@ -8,7 +9,13 @@ export default ({ label, name }) => {
         <label htmlFor={id} className="flex gap-4 px-6 py-3  border-[1px] border-grey-medium rounded-lg">
             <div 
                 className="h-6 w-6 border-2 rounded-full border-grey-medium shrink-0 has-[:checked]:border-transparent">
-                <input id={id} name={name} className="hidden peer" type="radio" />
+                <Field
+                    value={label}
+                    id={id}
+                    name={name}
+                    className="hidden peer"
+                    type="radio"
+                />
                 <Image
                     className="hidden peer-checked:block"
                     alt=""
