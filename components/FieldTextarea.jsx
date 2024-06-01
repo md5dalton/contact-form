6bb936ced42fd804fa4eb245c0ee71fc161ad9e6
md5dalton <md5daltonggs@gmail.com>
2024-06-01 18:36:09 +0200

    Update FieldTextarea.jsx

--- components/FieldTextarea.jsx
+++ components/FieldTextarea.jsx
@@ -1,9 +1,11 @@
 import { gId } from "@/lib/functions"
 import FieldText from "./FieldText"
+import { Field } from "formik"
 
 export default ({ label }) => (
     <FieldText label={label}>
-        <textarea
+        <Field
+            component="textarea"
             name={gId(label)}
             className="outline-none border-[1px] border-grey-medium rounded-lg py-3 px-6 min-h-48"
         />
