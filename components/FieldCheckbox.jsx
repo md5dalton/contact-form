730aa78a5621a44dade9ecc9b724de19b4d51c36
md5dalton <md5daltonggs@gmail.com>
2024-06-01 18:36:12 +0200

    Update FieldCheckbox.jsx

--- components/FieldCheckbox.jsx
+++ components/FieldCheckbox.jsx
@@ -1,12 +1,18 @@
 import Image from "next/image"
 import Fieldset from "./Fieldset"
+import { Field } from "formik"
 
 export default ({ label, name }) => (
     <Fieldset name={name}>
         <label htmlFor={name} className="flex gap-4 items-center">
             <div
                 className="h-6 w-6 border-2 rounded-sm border-grey-medium shrink-0 has-[:checked]:border-transparent">
-                <input id={name} name={name} className="hidden peer" type="checkbox" />
+                <Field
+                    id={name}
+                    name={name}
+                    className="hidden peer"
+                    type="checkbox"
+                />
                 <Image
                     className="hidden peer-checked:block"
                     alt=""
