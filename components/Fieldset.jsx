bee0d9d70b86c9088010c176d92f25694b47c3ca
md5dalton <md5daltonggs@gmail.com>
2024-06-01 17:45:37 +0200

    Update Fieldset.jsx

--- components/Fieldset.jsx
+++ components/Fieldset.jsx
@@ -1,6 +1,10 @@
-export default ({ children }) => (
+import { gId } from "@/lib/functions"
+import { ErrorMessage } from "formik"
+
+export default ({ children, name }) => (
     <fieldset className="grid gap-y-2">
     {/* <fieldset className="grid gap-y-2 grid-rows-[auto_24px]"> */}
         {children}
+        <ErrorMessage className="text-red text-s" name={gId(name)} component="p" />
     </fieldset>
 )
