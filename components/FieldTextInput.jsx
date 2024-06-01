adacf88d392b70d56a88c37a8f14cefc71e3d1b2
md5dalton <md5daltonggs@gmail.com>
2024-06-01 10:32:29 +0200

    Update FieldTextInput.jsx

--- components/FieldTextInput.jsx
+++ components/FieldTextInput.jsx
@@ -1,7 +1,7 @@
 import FieldText from "./FieldText"
 
-export default ({ label }) => (
+export default ({ label, type="text" }) => (
     <FieldText label={label}>
-        <input className="outline-none border-[1px] border-grey-medium rounded-lg py-3 px-6" type="text" />
+        <input className="outline-none border-[1px] border-grey-medium rounded-lg py-3 px-6" type={type} />
     </FieldText>
 )
