49b6cb8b7c109bbb5bbeeb5e80a6475bb0818513
md5dalton <md5daltonggs@gmail.com>
2024-06-01 11:07:53 +0200

    Update FieldCheckbox.jsx

--- components/FieldCheckbox.jsx
+++ components/FieldCheckbox.jsx
@@ -1,16 +1,31 @@
+import Image from "next/image"
 import ErrorMsg from "./ErrorMsg"
 import Fieldset from "./Fieldset"
 
-export default ({ label, children }) => (
-    <Fieldset>
-        <div className="flex gap-4 items-center">
-            <div className="h-6 w-6 border-2 rounded-sm border-grey-medium shrink-0"></div>
-            <div className="space-x-2">
-                <label>{label}</label>
-                <span className="text-green-medium">*</span>
-            </div>
-            <input className="hidden" type="checkbox" />
-        </div>
-        <ErrorMsg>This field is required</ErrorMsg>
-    </Fieldset>
-)
+export default ({ label }) => {
+    
+    const id = label.toLowerCase().replaceAll(" ", "-")
+    
+    return (
+        <Fieldset>
+            <label htmlFor={id} className="flex gap-4 items-center">
+                <div
+                    className="h-6 w-6 border-2 rounded-sm border-grey-medium shrink-0 has-[:checked]:border-transparent">
+                    <input id={id} className="hidden peer" type="checkbox" />
+                    <Image
+                        className="hidden peer-checked:block"
+                        alt=""
+                        height={24}
+                        width={24}
+                        src="/images/icon-checkbox-check.svg"
+                    />
+                </div>
+                <div className="space-x-2">
+                    <span>{label}</span>
+                    <span className="text-green-medium">*</span>
+                </div>
+            </label>
+            <ErrorMsg>This field is required</ErrorMsg>
+        </Fieldset>
+    )
+}
