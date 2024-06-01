19c46dfc141d3d3aef3938b76a4649d835ac2107
md5dalton <md5daltonggs@gmail.com>
2024-06-01 11:51:09 +0200

    Update Fieldset.jsx

--- components/Fieldset.jsx
+++ components/Fieldset.jsx
@@ -1,5 +1,6 @@
 export default ({ children }) => (
-    <fieldset className="grid gap-y-2 grid-rows-[auto_24px]">
+    <fieldset className="grid gap-y-2">
+    {/* <fieldset className="grid gap-y-2 grid-rows-[auto_24px]"> */}
         {children}
     </fieldset>
 )
