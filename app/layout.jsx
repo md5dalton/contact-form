b2a85fd0ac22f7e0869fd7537ae6873c30eb086a
md5dalton <md5daltonggs@gmail.com>
2024-08-06 15:22:19 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,14 +4,20 @@ import "./globals.css"
 const font = Karla({ subsets: ["latin"] })
 
 export const metadata = {
-  title: "Contact form",
-  description: "Frontend Mentor - Contact form",
+    title: "Contact form",
+    description: "Frontend Mentor - Contact form",
 }
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en" className="bg-green-lighter">
-      <body className={font.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en" className="bg-green-lighter">
+            <body className={font.className}>
+                <main
+                    className="px-4 my-8 mx-auto max-w-[690px]"
+                >
+                    {children}
+                </main>
+            </body>
+        </html>
+    )
 }
